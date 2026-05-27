f277b5ffa55b2db8d327b41333ce753953d5246a
m.bianchi <m.bianchi@patente.it>
2026-05-27 10:33:20 +0200

    Add basename configuration to router and enable GitToolBox settings

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,24 +4,27 @@ import { ProjectsPage } from './pages/Projects.page';
 import { SkillsPage } from './pages/Skills.page';
 import { ContactPage } from './pages/Contact.page';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/projects',
-    element: <ProjectsPage />,
-  },
-  {
-    path: '/skills',
-    element: <SkillsPage />,
-  },
-  {
-    path: '/contact',
-    element: <ContactPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/projects',
+      element: <ProjectsPage />,
+    },
+    {
+      path: '/skills',
+      element: <SkillsPage />,
+    },
+    {
+      path: '/contact',
+      element: <ContactPage />,
+    },
+  ],
+  { basename: '/portfolio' }
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
